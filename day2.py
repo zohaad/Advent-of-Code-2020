6168bab5f4f22656ebc3d0d53e0b94598e8555c7
Zohaad Fazal <10496606+zohaad@users.noreply.github.com>
2020-12-03 21:17:10 +0100

    initial commit

---
+++ day2.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+from functools import reduce
+
+lines = Path('inputs/2.txt').read_text().split('\n')
+
+def destruct(line):
+    policy, password = line.split(': ')
+    counts, letter = policy.split(' ')
+    min_count, max_count = map(int, counts.split('-'))
+
+    return password, letter, min_count, max_count
+
+def valid(line):
+    # destruct
+    password, letter, min_count, max_count = destruct(line)
+
+    # check if valid password
+    return min_count <= password.count(letter) <= max_count
+
+def valid2(line):
+    password, letter, a, b = destruct(line)
+    return (password[a - 1] == letter) ^ (password[b - 1] == letter)
+
+
+print('part 1:', sum(map(valid, lines)))
+print('part 2:', sum(map(valid2, lines)))
---
+++ day3.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+from operator import mul
+from functools import reduce
+
+tree_map = [[c for c in row] for row in Path('inputs/3.txt').read_text().split('\n')]
+
+tree_count = 0
+j = 0 # position
+height = len(tree_map)
+width = len(tree_map[0])
+
+for i in range(1, height):
+    j = (j + 3) % width
+    if tree_map[i][j] == '#':
+        tree_count += 1
+
+print('part 1:', tree_count)
+
+# part 2
+def count_trees(tree_map, right, down):
+    tree_count = 0
+    j = 0 # starting column position
+    height = len(tree_map)
+    width = len(tree_map[0])
+
+    for i in range(down, height, down):
+        j = (j + right) % width
+        if tree_map[i][j] == '#':
+            tree_count += 1
+    return tree_count
+
+slopes = [
+    [1, 1],
+    [3, 1],
+    [5, 1],
+    [7, 1],
+    [1, 2],
+]
+
+print('part 2:', reduce(mul, (count_trees(tree_map, *slope) for slope in slopes)))
